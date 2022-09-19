c8b3e970f0a8f679f08fadac4ce323b544345a84
Chris <81079621+chrisstar30@users.noreply.github.com>
2022-09-19 13:16:55 +0800

    Feat: Navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="navbar">
-    
+    <div class="container">
+      <div class="logo">HOMEWORK</div>
+      <div class="tools">
+        <ul>
+          <li>新增任務</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +16,20 @@
 </script>
 
 <style lang="sass" scoped>
-
+.navbar
+  width: 100%
+.container
+  padding: 8px 50px
+  display: flex
+  justify-content: space-between
+  align-items: center 
+  box-shadow: 0 3px 10px rgba(#000, .4)
+.logo
+  font-size: 1.25rem
+  font-weight: 600
+li
+  font-size: 1.125rem
+  display: block
+  user-select: none
+  cursor: pointer
 </style>
